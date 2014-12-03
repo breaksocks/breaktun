d6440b628560fcb20be5d2e872138afd946ef514
breakthat <breaksocks@gmail.com>
2014-12-03 22:11:36 +0800

    retryreq

--- tunnel/utils.go
+++ tunnel/utils.go
@@ -1,5 +1,12 @@
 package tunnel
 
+import (
+	"errors"
+	"io"
+	"net"
+	"time"
+)
+
 func WriteN2(bs []byte, offset int, n uint16) {
 	bs[offset] = byte(n >> 8)
 	bs[offset+1] = byte(n & 0xFF)
@@ -23,4 +30,34 @@ func ReadN4(bs []byte, offset int) uint32 {
 	n |= uint32(bs[offset+2]) << 8
 	n |= uint32(bs[offset+3])
 	return n
+}
+
+type RetryRequester struct {
+	read, write chan []byte
+	maxRetry    int
+	timeout     int
+}
+
+func NewRetryRequester(max_retry, timeout int, read, write chan []byte) *RetryRequester {
+	return &RetryRequester{
+		read: read, write: write,
+		maxRetry: max_retry, timeout: timeout,
+	}
+}
+
+var RequestTimeout error = errors.New("request timeout")
+
+func (req *RetryRequester) Request(data []byte) ([]byte, error) {
+	for i := 0; i < req.maxRetry; i += 1 {
+		req.write <- data
+		select {
+		case rep, ok := <-req.read:
+			if ok {
+				return rep, nil
+			}
+			return nil, io.EOF
+		case <-time.After(time.Millisecond * req.timeout):
+		}
+	}
+	return nil, RequestTimeout
 }
